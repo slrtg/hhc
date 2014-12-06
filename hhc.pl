9fe9147d1efece4c0cf6dfc236592aa9d42b192d
Saul Artiga <hellosaulartiga@gmail.com>
2014-12-06 01:18:26 -0800

    added unzip subroutine

--- hhc.pl
+++ hhc.pl
@@ -1,8 +1,10 @@
 #!usr/bin/perl
 use 5.016;
-use IO::Uncompress::Unzip qw(unzip $UnzipError);
+use IO::File;
+use IO::Uncompress::Unzip qw($UnzipError);
 use File::Copy qw(copy);
-use File::Remove qw(remove);
+use File::Path qw(mkpath);
+use File::Spec::Functions qw(splitpath);
 
 
 my $zipped_diag;
@@ -53,13 +55,75 @@ copy ("$zipped_diag", "$unzipped_diag/$zipped_diag");
 chdir ("$unzipped_diag") or die "$!";
 
 
+#Unzip and delete the zipped diagnostic from the new directory
+unzip($zipped_diag);
+unlink $zipped_diag;
 
-#Unzip the relevant file and do stuff
 
-my $diag_folder = new IO::Uncompress::Unzip $zipped_diag or die "Cannot open $zipped_diag: $UnzipError";
-die "Zipfile has no members" if ! defined $diag_folder->getHeaderInfo;
-for (my $status = 1; $status > 0; $status = $diag_folder->nextStream){
-	my $name = $diag_folder->getHeaderInfo->{Name};
-	warn "Processing member $name\n";
-	unzip $zipped_diag => $name, Name => $name or die "unzip failed: $UnzipError\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#A rewritten unzip function found on github
+
+
+
+sub unzip {
+    my ($file, $dest) = @_;
+ 
+    die 'Need a file argument' unless defined $file;
+    $dest = "." unless defined $dest;
+ 
+    my $u = IO::Uncompress::Unzip->new($file)
+        or die "Cannot open $file: $UnzipError";
+ 
+    my $status;
+    for ($status = 1; $status > 0; $status = $u->nextStream()) {
+        my $header = $u->getHeaderInfo();
+        my (undef, $path, $name) = splitpath($header->{Name});
+        my $destdir = "$dest/$path";
+ 
+        unless (-d $destdir) {
+            mkpath($destdir) or die "Couldn't mkdir $destdir: $!";
+        }
+ 
+        if ($name =~ m!/$!) {
+            last if $status < 0;
+            next;
+        }
+ 
+        my $destfile = "$dest/$path/$name";
+        my $buff;
+        my $fh = IO::File->new($destfile, "w")
+            or die "Couldn't write to $destfile: $!";
+        while (($status = $u->read($buff)) > 0) {
+            $fh->write($buff);
+        }
+        $fh->close();
+        my $stored_time = $header->{'Time'};
+        utime ($stored_time, $stored_time, $destfile)
+            or die "Couldn't touch $destfile: $!";
+    }
+ 
+    die "Error processing $file: $!\n"
+        if $status < 0 ;
+ 
+    return;
 }
